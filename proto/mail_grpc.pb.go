bd61607277337df4fb14ada6168b43816cd775e9
suminas-dev <81244389+suminas-dev@users.noreply.github.com>
2022-06-02 19:25:39 +0300

    Add files via upload

---
+++ proto/mail_grpc.pb.go
@@ -0,0 +1,228 @@
+package proto
+
+import (
+	context "context"
+
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+)
+
+const _ = grpc.SupportPackageIsVersion7
+
+type MailingListServiceClient interface {
+	CreateEmail(ctx context.Context, in *CreateEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
+	GetEmail(ctx context.Context, in *GetEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
+	UpdateEmail(ctx context.Context, in *UpdateEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
+	DeleteEmail(ctx context.Context, in *DeleteEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
+	GetEmailBatch(ctx context.Context, in *GetEmailBatchRequest, opts ...grpc.CallOption) (*GetEmailBatchResponse, error)
+}
+
+type mailingListServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewMailingListServiceClient(cc grpc.ClientConnInterface) MailingListServiceClient {
+	return &mailingListServiceClient{cc}
+}
+
+func (c *mailingListServiceClient) CreateEmail(ctx context.Context, in *CreateEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error) {
+	out := new(EmailResponse)
+	err := c.cc.Invoke(ctx, "/proto.MailingListService/CreateEmail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mailingListServiceClient) GetEmail(ctx context.Context, in *GetEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error) {
+	out := new(EmailResponse)
+	err := c.cc.Invoke(ctx, "/proto.MailingListService/GetEmail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mailingListServiceClient) UpdateEmail(ctx context.Context, in *UpdateEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error) {
+	out := new(EmailResponse)
+	err := c.cc.Invoke(ctx, "/proto.MailingListService/UpdateEmail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mailingListServiceClient) DeleteEmail(ctx context.Context, in *DeleteEmailRequest, opts ...grpc.CallOption) (*EmailResponse, error) {
+	out := new(EmailResponse)
+	err := c.cc.Invoke(ctx, "/proto.MailingListService/DeleteEmail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mailingListServiceClient) GetEmailBatch(ctx context.Context, in *GetEmailBatchRequest, opts ...grpc.CallOption) (*GetEmailBatchResponse, error) {
+	out := new(GetEmailBatchResponse)
+	err := c.cc.Invoke(ctx, "/proto.MailingListService/GetEmailBatch", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+type MailingListServiceServer interface {
+	CreateEmail(context.Context, *CreateEmailRequest) (*EmailResponse, error)
+	GetEmail(context.Context, *GetEmailRequest) (*EmailResponse, error)
+	UpdateEmail(context.Context, *UpdateEmailRequest) (*EmailResponse, error)
+	DeleteEmail(context.Context, *DeleteEmailRequest) (*EmailResponse, error)
+	GetEmailBatch(context.Context, *GetEmailBatchRequest) (*GetEmailBatchResponse, error)
+	mustEmbedUnimplementedMailingListServiceServer()
+}
+
+type UnimplementedMailingListServiceServer struct {
+}
+
+func (UnimplementedMailingListServiceServer) CreateEmail(context.Context, *CreateEmailRequest) (*EmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateEmail not implemented")
+}
+func (UnimplementedMailingListServiceServer) GetEmail(context.Context, *GetEmailRequest) (*EmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetEmail not implemented")
+}
+func (UnimplementedMailingListServiceServer) UpdateEmail(context.Context, *UpdateEmailRequest) (*EmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateEmail not implemented")
+}
+func (UnimplementedMailingListServiceServer) DeleteEmail(context.Context, *DeleteEmailRequest) (*EmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteEmail not implemented")
+}
+func (UnimplementedMailingListServiceServer) GetEmailBatch(context.Context, *GetEmailBatchRequest) (*GetEmailBatchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetEmailBatch not implemented")
+}
+func (UnimplementedMailingListServiceServer) mustEmbedUnimplementedMailingListServiceServer() {}
+
+type UnsafeMailingListServiceServer interface {
+	mustEmbedUnimplementedMailingListServiceServer()
+}
+
+func RegisterMailingListServiceServer(s grpc.ServiceRegistrar, srv MailingListServiceServer) {
+	s.RegisterService(&MailingListService_ServiceDesc, srv)
+}
+
+func _MailingListService_CreateEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailingListServiceServer).CreateEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.MailingListService/CreateEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailingListServiceServer).CreateEmail(ctx, req.(*CreateEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _MailingListService_GetEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailingListServiceServer).GetEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.MailingListService/GetEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailingListServiceServer).GetEmail(ctx, req.(*GetEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _MailingListService_UpdateEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailingListServiceServer).UpdateEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.MailingListService/UpdateEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailingListServiceServer).UpdateEmail(ctx, req.(*UpdateEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _MailingListService_DeleteEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailingListServiceServer).DeleteEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.MailingListService/DeleteEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailingListServiceServer).DeleteEmail(ctx, req.(*DeleteEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _MailingListService_GetEmailBatch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetEmailBatchRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailingListServiceServer).GetEmailBatch(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.MailingListService/GetEmailBatch",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailingListServiceServer).GetEmailBatch(ctx, req.(*GetEmailBatchRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var MailingListService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "proto.MailingListService",
+	HandlerType: (*MailingListServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "CreateEmail",
+			Handler:    _MailingListService_CreateEmail_Handler,
+		},
+		{
+			MethodName: "GetEmail",
+			Handler:    _MailingListService_GetEmail_Handler,
+		},
+		{
+			MethodName: "UpdateEmail",
+			Handler:    _MailingListService_UpdateEmail_Handler,
+		},
+		{
+			MethodName: "DeleteEmail",
+			Handler:    _MailingListService_DeleteEmail_Handler,
+		},
+		{
+			MethodName: "GetEmailBatch",
+			Handler:    _MailingListService_GetEmailBatch_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "proto/mail.proto",
+}
